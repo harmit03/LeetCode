6f62152614963220669397996d32f1ea485ceb59
harmit03 <harmitkatariya153@gmail.com>
2023-07-05 12:41:22 +0530

    added code to iterate through the 2d array and workout spiralmatrix

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -1,18 +1,40 @@
+import java.util.Arrays;
 
-
-class SpiralMatrix{
+class SpiralMatrix {
     public static void main(String[] args) {
 
-        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
         System.out.println(matrix.length);
-        
-        
+        int[] spiraled = spiralOrder(matrix);
+        System.out.println(Arrays.toString(spiraled));
+
     }
 
-    public int[] spiralOrder(int[][] matrix) {
-        int length = ((matrix[1].length)*(matrix.length));
+    public static int[] spiralOrder(int[][] matrix) {
+        int length = ((matrix[1].length) * (matrix.length));
         int[] arr = new int[length];
+        int index = 0;
+
+        while (true) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                arr[index] = matrix[0][j];
+                index++;
+            }
+            for (int j = 1; j < matrix.length; j++) {
+                arr[index] = matrix[j][matrix[j].length - 1];
+                index++;
+            }
+            for (int j = matrix[1].length - 2; j >= 0; j--) {
+                arr[index] = matrix[matrix.length - 1][j];
+                index++;
+            }
+            for (int j2 = matrix.length - 2; j2 > 0; j2--) {
+                arr[index] = matrix[j2][0];
+                index++;
+            }
+        }
+
         return arr;
     }
-    
+
 }
