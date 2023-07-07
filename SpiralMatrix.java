bcd61656ed0293c0a88fc3b35aeb6d0621e99ee6
harmit03 <harmitkatariya153@gmail.com>
2023-07-07 08:45:17 +0530

    conditioning

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -3,11 +3,21 @@ import java.util.Arrays;
 class SpiralMatrix {
     public static void main(String[] args) {
 
-        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int[][] matrix = { { 1, 2, 3, 5 },
+                { 4, 5, 6, 5 },
+                { 7, 8, 9, 5 },
+                { 10, 11, 12, 12 } };
         System.out.println(matrix.length);
         int[] spiraled = spiralOrder(matrix);
         System.out.println(Arrays.toString(spiraled));
 
+    }
+
+    public static boolean IsEmpty(int[] arr) {
+        if (arr[arr.length - 1] == 0) {
+            return true;
+        }
+        return false;
     }
 
     public static int[] spiralOrder(int[][] matrix) {
@@ -15,23 +25,59 @@ class SpiralMatrix {
         int[] arr = new int[length];
         int index = 0;
 
-        while (true) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                arr[index] = matrix[0][j];
-                index++;
+        int breadth = matrix[0].length;
+        int height = matrix.length;
+        int start = 0;
+        while (index < arr.length) {
+
+            if (IsEmpty(arr)) {
+                for (int j = start; j < breadth; j++) {
+                    arr[index] = matrix[start][j];
+                    if (index < arr.length - 1) {
+                        index++;
+                    } else {
+                        return arr;
+                    }
+                }
             }
-            for (int j = 1; j < matrix.length; j++) {
-                arr[index] = matrix[j][matrix[j].length - 1];
-                index++;
+
+            if (IsEmpty(arr)) {
+                for (int j = start + 1; j < height; j++) {
+                    arr[index] = matrix[j][height - 1];
+                    if (index < arr.length - 1) {
+                        index++;
+                    } else {
+                        return arr;
+                    }
+                }
             }
-            for (int j = matrix[1].length - 2; j >= 0; j--) {
-                arr[index] = matrix[matrix.length - 1][j];
-                index++;
+
+            if (IsEmpty(arr)) {
+                for (int j = breadth - 2; j >= 0; j--) {
+                    arr[index] = matrix[height - 1][j];
+                    if (index < arr.length - 1) {
+                        index++;
+                    } else {
+                        return arr;
+                    }
+                }
             }
-            for (int j2 = matrix.length - 2; j2 > 0; j2--) {
-                arr[index] = matrix[j2][0];
-                index++;
+
+            if (IsEmpty(arr)) {
+                for (int j2 = breadth - 2; j2 > 0; j2--) {
+                    arr[index] = matrix[j2][start];
+                    if (index < arr.length - 1) {
+                        index++;
+                    } else {
+                        return arr;
+                    }
+                }
             }
+
+            breadth--;
+            height--;
+            start++;
+
         }
 
         return arr;
